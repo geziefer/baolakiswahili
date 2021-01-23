55369742553e86d036a42fec2bc68b8c385d6680
Alexander Rühl <alexander.ruehl@syrocon.de>
2021-01-23 23:33:48 +0100

    mirror board for 2nd player

--- baolakiswahili/baolakiswahili.view.php
+++ baolakiswahili/baolakiswahili.view.php
@@ -41,16 +41,31 @@
 
         /*********** Place your code below:  ************/
 
+        // next actions will fill this block from template
         $this->page->begin_block( "baolakiswahili_baolakiswahili", "circle" );
 
+        // get 2 players and their order
         list( $player1, $player2 ) = array_keys( $players );
 
-        // create 4 rows with 8 fields, scaling and shifting them according to the board perspective
-        self::createLine(1, 100, 100, 92, 28, $player2, False);
-        self::createLine(2, 104, 100, 76, 30, $player2, True);
-        self::createLine(3, 108, 100, 62, 49, $player1, True);
-        self::createLine(4, 112, 100, 46, 68, $player1, False);
+        // check if current player is first (2nd will get mirrored board)
+        global $g_user;
+        $isFirst = (array_keys($players)[0] == $g_user->get_id());
 
+        // create 4 rows with 8 fields, scaling and shifting them according to the board perspective
+        if ($isFirst)
+        {
+          self::createLine(1, 100, 100, 92, 28, $player2, 16, -1);
+          self::createLine(2, 104, 100, 76, 30, $player2, 1, +1);
+          self::createLine(3, 108, 100, 62, 49, $player1, 1, +1);
+          self::createLine(4, 112, 100, 46, 68, $player1, 16, -1);
+        }
+        else
+        {
+          self::createLine(1, 100, 100, 92, 28, $player1, 9, +1);
+          self::createLine(2, 104, 100, 76, 30, $player1, 8, -1);
+          self::createLine(3, 108, 100, 62, 49, $player2, 8, -1);
+          self::createLine(4, 112, 100, 46, 68, $player2, 9, +1);
+        }
 
 
         /*
@@ -97,9 +112,9 @@
     /*
     Put circle in each field of a line and applies shifts and scale.
     */
-    function createLine($y, $hor_scale, $ver_scale, $hor_shift, $ver_shift, $player, $up) 
+    function createLine($y, $hor_scale, $ver_scale, $hor_shift, $ver_shift, $player, $startField, $increment) 
     {
-      $field = ($up) ? 1 : 16;
+      $field = $startField;
       for( $x=1; $x<=8; $x++ )
       {
           // insert an invisible circle for later clicking with ID according to board in DB
@@ -110,7 +125,7 @@
             'TOP' => round( ($y-1)*$ver_scale+$ver_shift )
           ) );
 
-       $field += ($up) ? +1 : -1;
+       $field += $increment;
       }
     }      
 
